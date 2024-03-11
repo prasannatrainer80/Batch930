f59812d51b9d8ec1270dde4d896825f907cf43bb
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2024-03-11 11:24:32 +0530

    code

---
+++ March11/ServletDemos/src/main/java/com/java/serv/AttendanceServlet.java
@@ -0,0 +1,45 @@
+package com.java.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AttendanceServlet
+ */
+public class AttendanceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AttendanceServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String[] values = request.getParameterValues("chkAttendance");
+		PrintWriter out = response.getWriter();
+		for (String s : values) {
+			out.println(s + "<br/>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
