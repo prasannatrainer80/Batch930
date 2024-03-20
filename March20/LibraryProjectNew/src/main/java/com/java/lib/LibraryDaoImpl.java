b6bb24fd35e62bd66f38f613118c53430800ddb6
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2024-03-20 11:23:20 +0530

    code

---
+++ March20/LibraryProjectNew/src/main/java/com/java/lib/LibraryDaoImpl.java
@@ -0,0 +1,49 @@
+package com.java.lib;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LibraryDaoImpl implements LibraryDao {
+
+	Connection connection;
+	PreparedStatement pst;
+	
+	public int userExists(String user) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select count(*) cnt from libusers where userName=?";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, user);
+		ResultSet rs = pst.executeQuery();
+		rs.next();
+		int count = rs.getInt("cnt");
+		return count;
+	}
+	@Override
+	public String createUser(String user, String password) throws ClassNotFoundException, SQLException {
+		if (userExists(user)==1) {
+			return "User-Name Already Exists...";
+		}
+		connection = ConnectionHelper.getConnection();
+		String cmd = "Insert into libusers(Username,Password) values(?, ?)";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, user);
+		pst.setString(2, password);
+		pst.executeUpdate();
+		return "Login Created Successfully...";
+	}
+	@Override
+	public int loginCheck(String user, String password) throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select count(*) cnt from libusers where username = ? AND Password = ?";
+		pst = connection.prepareStatement(cmd);
+		pst.setString(1, user);
+		pst.setString(2, password);
+		ResultSet rs = pst.executeQuery();
+		rs.next();
+		int count = rs.getInt("cnt");
+		return count;
+	}
+
+}
