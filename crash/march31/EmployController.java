91d9c34048d6c3e7ffeeb29a1eec1b1d4f98295f
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2024-03-31 12:20:08 +0530

    code

--- crash/march31/EmployController.java
+++ crash/march31/EmployController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class EmployController {
@@ -15,5 +20,16 @@ public class EmployController {
 	@GetMapping(value="/showEmploy")
 	public List<Employ> showEmploy() {
 		return impl.showEmployDao();
+	}
+	
+	@GetMapping(value="/searchEmploy/{empno}")
+	public Employ searchEmploy(@PathVariable int empno) {
+		return impl.searchEmploy(empno);
+	}
+	
+	@PostMapping("/addEmploy")
+	public String add(@RequestBody Employ employ) {
+		 impl.addEmploy(employ);
+		 return "Employ Record Inserted...";
 	}
 }
--- crash/march31/EmployDaoImpl.java
+++ crash/march31/EmployDaoImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.net.ConnectException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -34,6 +35,39 @@ public class EmployDaoImpl implements EmployDao {
 			
 		});
 		return employList;
+	}
+
+	@Override
+	public Employ searchEmploy(int empno) {
+		String cmd = "select * from Employ where empno = ?";
+		List<Employ> employList = jdbcTemplate.query(cmd, new Object[] {empno},
+				new RowMapper<Employ>() {
+
+			@Override
+			public Employ mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Employ employ = new Employ();
+				employ.setEmpno(rs.getInt("empno"));
+				employ.setName(rs.getString("name"));
+				employ.setGender(rs.getString("gender"));
+				employ.setDept(rs.getString("dept"));
+				employ.setDesig(rs.getString("desig"));
+				employ.setBasic(rs.getDouble("basic"));
+				return employ;
+			}
+			
+		});
+		return employList.get(0);
+	}
+
+	@Override
+	public String addEmploy(Employ employ) {
+		String cmd = "Insert into Employ(empno, name, gender, dept, desig, "
+				+ "basic) values(?, ?, ?, ?, ?)";
+		jdbcTemplate.update(cmd, new Object[] {employ.getEmpno(), employ.getName(),
+			employ.getGender(), employ.getDept(), employ.getDesig(),
+				employ.getBasic()
+				});
+		return "Employ Record Inserted...";
 	}
 
 }
