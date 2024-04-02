57783b181365762e0b32a8fcbbce35dd912858f8
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2024-04-02 11:18:23 +0530

    code

---
+++ April2/CmsHib/src/main/java/com/java/hib/Menu.java
@@ -0,0 +1,59 @@
+package com.java.hib;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Menu")
+public class Menu {
+
+	@Id
+	private int menuId;
+	private int restaurantId;
+	private String itemName;
+	private double price;
+	private String speciality;
+	public int getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(int menuId) {
+		this.menuId = menuId;
+	}
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getSpeciality() {
+		return speciality;
+	}
+	public void setSpeciality(String speciality) {
+		this.speciality = speciality;
+	}
+	public Menu(int menuId, int restaurantId, String itemName, double price, String speciality) {
+		this.menuId = menuId;
+		this.restaurantId = restaurantId;
+		this.itemName = itemName;
+		this.price = price;
+		this.speciality = speciality;
+	}
+	public Menu() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
---
+++ April2/CmsHib/src/main/webapp/RestaurantShow.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<table border="3" align="center">
+		<tr>
+			<th>Restaurant Id</th>
+			<th>Resturant Name</th>
+			<th>City</th>
+			<th>Location</th>
+			<th>Rating</th>
+			<th>Show Menu</th>
+		</tr>
+	<jsp:useBean id="beanRestaurantDao" class="com.java.hib.RestaurantDaoImpl" />
+	<c:set var="restaurantList" value="${beanRestaurantDao.showRestaurantDao()}" />
+	<c:forEach var="restaurant" items="${restaurantList}">
+		<tr>
+			<td><c:out value="${restaurant.restaurantId}" /> </td>
+			<td><c:out value="${restaurant.restaurantName}" /> </td>
+			<td><c:out value="${restaurant.city}" /> </td>
+			<td><c:out value="${restaurant.location}" /> </td>
+			<td><c:out value="${restaurant.rating}" /> </td>
+			<td><a href="MenuShow.jsp">Menu</a> </td>
+		</tr>
+	</c:forEach>
+	</table>
+</body>
+</html>
