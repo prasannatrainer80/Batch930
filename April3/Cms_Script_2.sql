ee4ab72d2f4d3d337455a40d2c216fbbd432af00
prasannatrainer80 <prasanna.java.trainer@gmail.com>
2024-04-03 10:04:39 +0530

    code

--- April3/Cms_Script_2.sql
+++ April3/Cms_Script_2.sql
@@ -23,3 +23,14 @@ INSERT INTO `customer` VALUES (1,'Prasanna Pappu','9381413089','prassucp','hexaw
 (9022,'kjsdlfak','88848483','sdfkjj','kkaskf','a@b.com'),
 (9121,'Naresh','422555555','nareshps','infinite','naresh@gmail.com');
 
+DROP TABLE IF EXISTS `wallet`;
+CREATE TABLE `wallet` (
+  `CUS_ID` int unsigned DEFAULT NULL,
+  `WAL_ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `WAL_AMOUNT` double unsigned NOT NULL,
+  `WAL_SOURCE` enum('PAYTM','CREDIT_CARD','DEBIT_CARD') DEFAULT 'DEBIT_CARD',
+  PRIMARY KEY (`WAL_ID`),
+  UNIQUE KEY `CUS_ID` (`CUS_ID`,`WAL_SOURCE`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `wallet` VALUES (1,1,4215,'PAYTM'), (1,2,3000,'DEBIT_CARD'),  (1,3,5602.36,'CREDIT_CARD'),  (2,4,4500.36,'DEBIT_CARD'), 
+(3,5,485.23,'CREDIT_CARD'),  (2,6,3000,'CREDIT_CARD'),  (2,7,4560,'PAYTM'),   (3,8,56.36,'PAYTM'),  (3,9,4566,'DEBIT_CARD');
